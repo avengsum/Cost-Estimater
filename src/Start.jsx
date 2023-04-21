e3c480cfc123911791108f3fdb56439384281bee
ANWDM <pv4003337@gmail.com>
2023-04-22 00:39:16 +0530

    start page done

--- src/Start.jsx
+++ src/Start.jsx
@@ -1,9 +1,13 @@
-const Start = () => {
+const Start = ( ) => {
     return (
         <div>
             <h1>
             Welcome to the challenge
             </h1>
+            <p>You will be presented with 3 MCQs.</p>
+            <p>You have to solve it in give amount of time</p>
+
+            <button>Start Quiz</button>
         </div>
     )
 }
